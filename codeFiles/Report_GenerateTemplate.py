35ca5eda59cb970e24affd573a1471fbe6f854b2
RealAks <98399485+RealAks@users.noreply.github.com>
2022-01-27 23:33:58 +0530

    Update Report_GenerateTemplate.py

--- codeFiles/Report_GenerateTemplate.py
+++ codeFiles/Report_GenerateTemplate.py
@@ -1,6 +1,7 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
+#Generating Template
 def getSectionHeader(row, isColHead, colStart):
     splitString = []
     sectionH = ""
